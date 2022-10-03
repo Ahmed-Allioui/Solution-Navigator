5a935da99c7bacf1227af99cfcb01482f6bc88bf
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-03 21:49:58 +0200

    team path added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/App-Home.vue'
 import About from '../views/App-About.vue'
+import Task from '../components/App-Task.vue'
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
@@ -12,6 +13,10 @@ const router = createRouter({
         {
             path: '/about',
             component: About
+        },
+        {
+            path: '/team',
+            component: Task
         }
     ]
 })
