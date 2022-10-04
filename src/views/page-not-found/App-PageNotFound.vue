533e15119c03cb5de2d18b0b4d0754a10b2b885f
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-04 15:01:22 +0200

    Page not found vue created

---
+++ src/views/page-not-found/App-PageNotFound.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    <div class="container">
+      <img src="./page-not-found.png" alt="404" />
+      <p>Sorry, Seite nicht gefunden!</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.main {
+    padding: 0%;
+    display: flex;
+    justify-content: center;
+    margin-top: 15%;
+  .container {
+    
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    img {
+        font-size:xx-small;
+        padding-right: 2rem;
+    }
+
+    p {
+        font-size: 1.5rem;
+    }
+  }
+}
+</style>
