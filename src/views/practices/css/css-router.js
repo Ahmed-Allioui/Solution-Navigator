2ad8e1921ceecf2479cdc6f6f831acec191a39e2
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-04 15:02:32 +0200

    css route added

---
+++ src/views/practices/css/css-router.js
@@ -0,0 +1,13 @@
+import Task from '../../../views/practices/css/CSS-Practice.vue'
+
+const routers =
+    [
+        {
+            path: 'something',
+            component: Task,
+            title: 'Übung',
+            name: 'css-something'
+        }
+]
+
+export default routers
