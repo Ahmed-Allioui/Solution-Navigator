56b6580d9adabd1dd039d58f7e3802ab3127d638
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-04 15:00:26 +0200

    App task navbar button created

---
+++ src/components/task-component/App-Task-Navbar-Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <routerLink class="button" :to="to">
+        <span class="text">{{ text }}</span>
+    </routerLink>
+</template>
+
+<script>
+    export default {
+        props: {
+            to: { type: String, default: '/pagenotfound' },
+            text: { type: String, default: 'click me' },
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+
+.button {
+    padding: 0.7rem 1.5rem;
+    text-decoration: none;
+
+    .text {
+    color: var(--light);
+    }
+
+    &:hover, &.router-link-exact-active {
+        background-color: var(--dark);
+        border-bottom: 5px solid var(--primary);
+
+        .text {
+            color: var(--primary);
+        }
+    }
+}
+</style>
