5b283eddeafa3d11aca27dfeab828d1435aa4b6c
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-04 15:02:22 +0200

    CSS practice vue added

---
+++ src/views/practices/css/CSS-Practice.vue
@@ -0,0 +1,18 @@
+<template>
+    <AppTask :tabs="cssRouter"/>
+</template>
+
+<script>
+import AppTask from '../../../components/task-component/App-Task.vue'
+import cssRouter from '../../../views/practices/css/css-router'
+export default {
+    data() {
+        return {
+            cssRouter: cssRouter
+        }
+    },
+    components: {
+        AppTask
+    }
+};
+</script>
