6bfb9d86f7a89700fe0567bbea133504a0b86ed3
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-07 22:01:45 +0200

    HTTP-Practices vue

---
+++ src/views/practices/01-http-uri-html/HTTP-Practice.vue
@@ -0,0 +1,19 @@
+<template>
+    <AppTask :tabs="routes"/>
+</template>
+
+<script>
+import AppTask from '../../../components/task-component/App-Task.vue'
+import routes from '../../../views/practices/01-http-uri-html/http-routes'
+
+export default {
+    data() {
+        return {
+            routes: routes
+        }
+    },
+    components: {
+        AppTask
+    }
+};
+</script>
