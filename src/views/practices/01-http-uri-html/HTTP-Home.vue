c8839ce55e5ad8933118251d076f32cc0fea6a22
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-07 21:59:59 +0200

    HTTP-Home.vue created

---
+++ src/views/practices/01-http-uri-html/HTTP-Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Übung 1: HTTP, URI und HTML</h1>
+  <h3>Lernziele</h3>
+  <ol>
+    <li>Die Geschichte des WWW beschreiben können.</li>
+    <li>Grundbegriffe des WWW erklären können.</li>
+    <li>Die Paradigmen der WWW-Technologien erläutern können.</li>
+    <li>Das HTTP-Protokoll beschreiben können.</li>
+    <li>Die wichtigsten HTTP-Fehlercodes kennen und erläutern können.</li>
+    <li>Uniform Resource Identifier (URI) und deren Anwendung erklären können.</li>
+    <li>HTML beschreiben und anwenden können.</li>
+    <li>Wireframes mit HTML nachbauen können.</li>
+  </ol>
+</template>
