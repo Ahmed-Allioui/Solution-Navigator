8dd80bbf20e6f6421f87cece408c16585692a1a5
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-07 21:59:07 +0200

    HTTP-A1.vue created

---
+++ src/views/practices/01-http-uri-html/HTTP-A1.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Fachliche Argumentation über Erfolgsprinzipien des WWW</h1>
+  <ol>
+    <li class="question">
+      Mit welchen fachlichen Argumenten wurde das WWW-Proposal von TBL
+      abgelehnt?
+    </li>
+    <p>
+      Das WWW-Proposal von TBL wurde abgelehnt, weil das Komitee sich von
+      Hypertext was anderes vorgestellt hat, also etwas Sauberes, wo es keinen
+      Brokenlinks gibt und die Hyperlinks immer stimmen. Sie dachten, dass sie
+      schon weiter sind als der TBL auf seiner Papier schreibt.
+    </p>
+    <li class="question">
+      Was sind die fachlichen Argumente, warum das WWW dennoch ein Erfolg wurde?
+    </li>
+    <p>
+      Das WWW wurde dennoch wegen Offenheit, Dezentralität und Evolvierbarkeit
+      ein Erfolg. Also wichtig war es, dass sich die unterschiedlichen
+      Technologien aufgrund der Evolvierbarkeit orthogonal zueinander entwickeln würden.
+    </p>
+    <li class="question">
+      Was wäre der Preis für die garantierte Verhinderung von “broken links”?
+    </li>
+    <p>
+        Der Preis für die garantierte Verhinderung von “broken links” wäre:
+        <ul>
+            <li>ein zentrale Kontrolle</li>
+            <li>die Geschlossenheit</li>
+        </ul>
+    </p>
+  </ol>
+</template>
