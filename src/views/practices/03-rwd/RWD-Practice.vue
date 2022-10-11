e2c2c4c46cdd543a72e5c40aaf7db91c8595a597
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-11 12:52:32 +0200

    rdw practice vue created

---
+++ src/views/practices/03-rwd/RWD-Practice.vue
@@ -0,0 +1,19 @@
+<template>
+    <AppTask :tabs="routes"/>
+</template>
+
+<script>
+import AppTask from '../../../components/task-component/App-Task.vue'
+import routes from '../../../views/practices/03-rwd/rwd-routes'
+
+export default {
+    data() {
+        return {
+            routes: routes
+        }
+    },
+    components: {
+        AppTask
+    }
+};
+</script>
