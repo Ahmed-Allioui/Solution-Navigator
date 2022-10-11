b61dffab71c1b9b1fc3f9b3e0b4e967913bd5a41
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-11 12:47:58 +0200

    code replaced by code as image

--- src/views/practices/01-http-uri-html/HTTP-A4.vue
+++ src/views/practices/01-http-uri-html/HTTP-A4.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>HTML Wireframe</h1>
   <p class="question">Gegeben ist folgendes HTML-Wireframe:</p>
-  <img src="./picture-1.4.png" alt="" />
+  <img src="./picture-1.4.png" alt="" class="picture" />
   <p class="question">
     Mit welchem HTML-Code (ohne CSS, nur mit HTML-Tags) kann man diesen
     Wireframe exakt nachbilden? Die beiden Bilder finden Sie z.B. unter
@@ -15,94 +15,35 @@
     >.
   </p>
 
-  <h1>Übung 1.2: Inventors of the web</h1>
-  <ul>
-    <li><mark>Tim Berners-Lee</mark>: WWW, HTTP, HTML,URI</li>
-    <li>Hakom Lie and Bert Bos: CSS</li>
-    <li>Brendan Eich: JavaScript</li>
-  </ul>
-  <hr />
-  <br />
-  <h2>Inventors of the WWW</h2>
-  <table>
-    <tr>
-      <td colspan="4"><center>Inventors of the WWW</center></td>
-    </tr>
-    <tr>
-      <td><center>WWW</center></td>
-      <td><center>HTML</center></td>
-      <td><center>CSS</center></td>
-      <td><center>JavaScript</center></td>
-    </tr>
-    <tr>
-      <td>
-        <center><mark>Tim Berners-Lee</mark></center>
-      </td>
-      <td>
-        <center><mark>Tim Berners-Lee</mark></center>
-      </td>
-      <td><center>Hakom Lie and Bert Bos</center></td>
-      <td><center>Brendan Eich</center></td>
-    </tr>
-  </table>
-  <br />
-  <hr />
-  <table>
-    <thead>
-      <tr>
-        <th colspan="3"><center>Inventors of the WWW</center></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td colspan="3">
-          <center><hr /></center>
-        </td>
-      </tr>
-      <tr>
-        <td><center>HTML</center></td>
-        <td><center>|</center></td>
-        <td><center>JavaScript</center></td>
-      </tr>
-      <tr>
-        <td>
-          <center>
-            <img
-              src="https://kaul.inf.h-brs.de/ccm/we/ws22/resources/img/assets/tbl.jpg"
-              alt=""
-            />
-          </center>
-        </td>
-        <td><center>|</center></td>
-        <td>
-          <center>
-            <img
-              src="https://kaul.inf.h-brs.de/ccm/we/ws22/resources/img/assets/eich.jpg"
-              alt=""
-            />
-          </center>
-        </td>
-      </tr>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td>
-          <center><mark>Tim Berners-Lee</mark></center>
-        </td>
-        <td><center>|</center></td>
-        <td><center>Brendan Eich</center></td>
-      </tr>
-    </tfoot>
-  </table>
-  <br />
-  <hr />
+  <div class="rawHTML">
+    <h3>Code</h3>
+    <img src="./code.jpg" alt="Code">
+    <br>
+    <h3>Result</h3>
+    <img src="./result.jpg" alt="Result">
+  </div>
 </template>
 
-<style scoped>
-img {
-  width: 30%;
-  height: 30%;
+<style lang="scss" scoped>
+.picture {
+  max-width: 500px;
+  width: 100%;
+  height: auto;
   align-self: center;
-  padding: 2rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+}
+
+.rawHTML {
+  display: flex;
+  flex-direction: column;
+
+  
+  img {
+    border: 1px solid black;
+    align-self: center;
+    border-radius: 15px;
+    max-width: 500px;
+  }
 }
 </style>
