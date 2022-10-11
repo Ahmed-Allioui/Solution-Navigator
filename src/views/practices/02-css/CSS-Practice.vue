9f99898a09c6fe878b99f1cdda0b1a47ffd63e8d
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-11 12:50:28 +0200

    CSS Practice created and implemented

---
+++ src/views/practices/02-css/CSS-Practice.vue
@@ -0,0 +1,19 @@
+<template>
+    <AppTask :tabs="routes"/>
+</template>
+
+<script>
+import AppTask from '../../../components/task-component/App-Task.vue'
+import routes from '../../../views/practices/02-css/css-routes'
+
+export default {
+    data() {
+        return {
+            routes: routes
+        }
+    },
+    components: {
+        AppTask
+    }
+};
+</script>
