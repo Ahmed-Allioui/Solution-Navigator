1d1faebe394d8c9d02564086794ce2b18718b9a2
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-11 12:50:17 +0200

    CSS Home created and implemented

---
+++ src/views/practices/02-css/CSS-Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Übung 2: CSS</h1>
+    <h3>Lernziele</h3>
+    <ol>
+      <li>CSS beschreiben und anwenden können.</li>
+      <li>Spezifität von CSS-Selektoren berechnen können.</li>
+      <li>RGB in HSL und umgekehrt mit DevTools umrechnen können.</li>
+      <li>Wireframes mit HTML und CSS nachbauen können.</li>
+      <li>Eigene Homepage mit HTML und CSS erstellen können.</li>
+    </ol>
+  </template>
