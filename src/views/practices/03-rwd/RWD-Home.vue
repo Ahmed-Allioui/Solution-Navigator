83368dc6628b5d3f4d2baef7823220de7d947683
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-11 15:12:02 +0200

    rwd home vue implemented

---
+++ src/views/practices/03-rwd/RWD-Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Übung 3: Responsives Webdesign</h1>
+  <h3>Lernziele</h3>
+  <ol>
+    <li>Responsive Web Design (RWD) erläutern können.</li>
+    <li>Wireframes mit HTML und CSS responsiv nachbauen können.</li>
+    <li>Eigene Homepage mit HTML und CSS responsiv gestalten können.</li>
+    <li>Flexbox-Layout einsetzen können.</li>
+    <li>Grid Layout anwenden können.</li>
+    <li>
+      Die 10 technischen CSS-Mittel für Responsive Web Design (RWD) erläutern
+      und anwenden können.
+    </li>
+    <li>Wireframes mit Flexbox und Grid nachbauen können.</li>
+    <li>Eigene Homepage mit Flexbox und Grid gestalten können.</li>
+  </ol>
+</template>
