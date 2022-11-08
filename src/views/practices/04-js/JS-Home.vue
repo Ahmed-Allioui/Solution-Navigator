05b6c73f41a538b57096a20b8db43f49c244d52e
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-11-08 10:50:55 +0100

    js home implemented

---
+++ src/views/practices/04-js/JS-Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Übung 4: JavaScript (JS)</h1>
+  <h3>Lernziele</h3>
+  <ol>
+    <li>JavaScript-Konzepte erklären können.</li>
+    <li>JavaScript-Programme selbst erstellen können.</li>
+    <li>Kleine Event-Handler schreiben können.</li>
+    <li>Privatisierung in JavaScript programmieren können.</li>
+    <li>Douglas Crockford Power Constructor einsetzen können.</li>
+    <li>IIFE-Pattern in JavaScript programmieren können.</li>
+    <li>Prototypische Vererbung in JavaScript programmieren können.</li>
+    <li>
+      Mit Objekten und Funktionen in JavaScript komplexe Probleme lösen können.
+    </li>
+  </ol>
+</template>
