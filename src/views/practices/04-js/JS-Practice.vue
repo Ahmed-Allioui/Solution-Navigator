94c939536c811e8952d144a710cc17111d2d2c8d
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-11-08 10:51:11 +0100

    js practice vue implemented

---
+++ src/views/practices/04-js/JS-Practice.vue
@@ -0,0 +1,19 @@
+<template>
+    <AppTask :tabs="routes"/>
+</template>
+
+<script>
+import AppTask from '../../../components/task-component/App-Task.vue'
+import routes from '../../../views/practices/04-js/js-routes'
+
+export default {
+    data() {
+        return {
+            routes: routes
+        }
+    },
+    components: {
+        AppTask
+    }
+};
+</script>
