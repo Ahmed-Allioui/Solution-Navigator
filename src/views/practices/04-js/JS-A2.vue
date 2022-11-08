268f30108488a77d73c6dec245612d6577e882ce
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-11-08 10:50:25 +0100

    js a2 created

---
+++ src/views/practices/04-js/JS-A2.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            person_count: 2,
+            car_count: 2,
+            persons: new Set()
+        }
+    }
+}
+</script>
