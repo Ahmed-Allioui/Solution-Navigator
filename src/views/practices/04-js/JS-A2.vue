5e1b174c43ae64c3eab486050643f8e670d15c64
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-11-08 15:26:43 +0100

    js a2 implemented

--- src/views/practices/04-js/JS-A2.vue
+++ src/views/practices/04-js/JS-A2.vue
@@ -1,17 +1,137 @@
 <template>
     <div>
+        <label
+        >Geben Sie die Anzahl der Personen ein:
+        <input v-model="person_count"
+      /></label>
+    </div>
+    <br>
+    <div>
+        <label
+        >Geben Sie die Anzahl der Autos ein:
+        <input v-model="car_count"
+      /></label>
+    </div>
+    <br>
+    <div>
+        <button class="primary" @click="persons = generatePersons();">Neu anlegen</button>
+    </div>
+    <div class="container">
+        <div class="person-container" v-for="person in persons" :key="person.name">
+        <p>Name: {{person.name}}</p>
+        <p>Autos: </p>
+        <div class="car-container">
+            <label v-for="car in person.cars" :key="car.name">
+                <input type="checkbox" v-model="car.isSelected">  {{car.name}}
+            </label>
+        </div>
+    </div>
+    </div>
+    <div>
+        <button class="primary" @click="conflictExists = conflict()">verify conflicts</button>
+        <div class="message-container question">
+            <p v-if="conflictExists" class="error-message">{{ conflictMessage }}</p>
+            <p v-else class="success-message">{{ conflictMessage }}</p>
+        </div>
         
     </div>
 </template>
 
 <script>
+
 export default {
     data() {
         return {
-            person_count: 2,
-            car_count: 2,
-            persons: new Set()
+            person_count: 5,
+            car_count: 5,
+            persons : [],
+            conflictMessage : '',
+            conflictExists: false
+        }
+    },
+    created() {
+        this.persons = this.generatePersons();
+    },
+    methods: {
+        conflict() {
+            var carsInConflict = this.generateCars();
+            for(var person of this.persons) {
+                for(var [index, car] of person.cars.entries()) {
+                    if(car.isSelected && carsInConflict[index].isSelected) {
+                        this.conflictMessage = 'Das Auto [' + car.name + '] wurde mehmals besessen.'
+                        return true;
+                    }
+                    carsInConflict[index].isSelected |= car.isSelected;
+                }
+            }
+            this.conflictMessage = 'Es gibt keine Konflikte';
+            return false;
+        },
+        createPerson(id) {
+            return new Person('Person ' + id);
+        },
+        createCar(id) {
+            return new Car('Car ' + id);
+        },
+        generatePersons() {
+            var persons = [];
+            for(var i=0; i<this.person_count; i++) {
+                persons[i] = this.createPerson(i);
+                persons[i].cars = this.generateCars();
+            }
+            return persons;
+        },
+        generateCars() {
+            var cars = [];
+            for(var i=0; i<this.car_count; i++) {
+                cars[i] = this.createCar(i);
+            }
+            return cars;
         }
     }
 }
+
+function Person(name) {
+    this.name = name;
+    this.cars = [];
+}
+
+function Car(name) {
+    this.name = name;
+    this.isSelected = false;
+}
 </script>
+
+<style lang="scss" scoped>
+
+.container {
+    margin: 1vw;
+    display: flex;
+    flex-wrap: wrap;
+
+    .person-container {
+        margin: 1vw;
+        padding: 1vw;
+        border: 0.1rem solid;
+        border-radius: 0.5rem;
+
+        .car-container {
+            display: grid;
+        }
+    }
+}
+
+.message-container {
+    padding: 1vw;
+    .error-message {
+    color: red;
+}
+
+.success-message {
+    color: green;
+}
+}
+
+
+
+</style>
