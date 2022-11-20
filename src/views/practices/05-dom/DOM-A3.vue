0d752b47fe1c27a5600268ca98f9eeaacba4daab
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-11-20 21:54:55 +0100

    dom a3 implemented

---
+++ src/views/practices/05-dom/DOM-A3.vue
@@ -0,0 +1,11 @@
+<template>
+  <Topsort titel="TopSort als Web-App"/>
+</template>
+
+<script setup>
+import Topsort from "../04-js/JS-A4.vue";
+</script>
+  
+<style lang="scss" scoped>
+
+</style>
