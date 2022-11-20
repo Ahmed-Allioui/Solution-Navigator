40bc75353362a97f941cf1d91f6ab51ba9bbf363
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-11-20 21:55:26 +0100

    dom practice implemented

---
+++ src/views/practices/05-dom/DOM-Practice.vue
@@ -0,0 +1,19 @@
+<template>
+    <AppTask :tabs="routes"/>
+</template>
+
+<script>
+import AppTask from '../../../components/task-component/App-Task.vue'
+import routes from '../05-dom/dom-routes'
+
+export default {
+    data() {
+        return {
+            routes: routes
+        }
+    },
+    components: {
+        AppTask
+    }
+};
+</script>
