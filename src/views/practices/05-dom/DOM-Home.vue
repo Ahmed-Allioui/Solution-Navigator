f9ed277ba0172d5fb8ff3874825810c480b26e90
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-11-20 21:55:06 +0100

    dom view implemented

---
+++ src/views/practices/05-dom/DOM-Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Übung 5: Document Object Model (DOM)</h1>
+  <h3>Lernziele</h3>
+  <ol>
+    <li>Browser-Architektur erklären können.</li>
+    <li>Grundlegenden Ablauf des Renderings erklären können.</li>
+    <li>Document Object Model (DOM)-Konzepte erklären können.</li>
+    <li>In JavaScript DOM-Abfragen programmieren können.</li>
+    <li>In JavaScript DOM-Manipulationen programmieren können.</li>
+    <li>
+      Unterschiede zwischen innerHTML, innerText, textContent und outerHTML
+      erklären können.
+    </li>
+    <li>Performanz-Messungen von DOM-Operationen durchführen können.</li>
+  </ol>
+</template>
