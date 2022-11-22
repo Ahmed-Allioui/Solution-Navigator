b53ebd98c8ca33fb18835ce8c33615c1967257e6
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-11-22 15:33:49 +0100

    dom a2 implemented

--- src/views/practices/05-dom/DOM-A2.vue
+++ src/views/practices/05-dom/DOM-A2.vue
@@ -1,134 +1,161 @@
 <template>
   <div id="root"></div>
-  <h1>Rednerliste mit Zeitmessung</h1>
-  <h3>Rednerliste</h3>
-  <div class="input-container">
-    <label for="input"
-      >Neuer Redner
-      <input id="input" v-model="input" />
-    </label>
-    <button @click="addSpeaker(input)" class="primary">Hinzufügen</button>
-  </div>
-  <div class="speakers-container">
-    <ul>
-      <li v-for="(speaker, i) in speakers" :key="i">{{ speaker.name }}</li>
-    </ul>
-  </div>
 </template>
 
 <script>
-function Speaker(name) {
-  this.name = name;
-  this.seconds = new Date().getSeconds();
-  this.stoped = true;
-}
-const anton = new Speaker("Anton");
-const berta = new Speaker("Berta");
-const carl = new Speaker("Carl");
-var speakers = [anton, berta, carl];
 
-function initTimers() {
-  const d = new Date();
-  var timers = document.querySelectorAll(".timer");
-  for (let timer of timers) {
-    const i = timer.id;
-    speakers[i].seconds = d.getSeconds();
-    timer.textContent = subtractSeconds(d, speakers[i].seconds).toLocaleTimeString();
+var speakers = [];
+
+class Speaker {
+  constructor(name) {
+    this.name=name;
+    this.timer=new Date();
+    this.timer.setHours(0, 0, 0, 0);
+    this.stoped=true;
   }
 }
 
 function appTimer() {
-  const d = new Date();
   var timers = document.querySelectorAll(".timer");
   for (let timer of timers) {
     const i = timer.id;
-    if (speakers[i].stoped) {
-      speakers[i].seconds++;
-    } else {
-      timer.textContent = subtractSeconds(
-        d,
-        speakers[i].seconds
-      ).toLocaleTimeString();
+    if (!speakers[i].stoped) {
+      speakers[i].timer = addSecond(speakers[i].timer);
+      timer.textContent = speakers[i].timer.toLocaleTimeString()
     }
   }
 }
 
-function subtractSeconds(date, seconds) {
-  date.setSeconds(date.getSeconds() - seconds);
+function initSpeakers(ul) {
+  addSpeaker(ul, "Anton")
+  addSpeaker(ul, "Berta")
+  addSpeaker(ul, "Carl")
+}
 
+function spreakerDefaultContainer() {
+  const div = document.createElement("div");
+  div.style.display = "flex";
+  div.style.flexDirection = "row";
+  div.style.alignItems = "center"
+  div.style.gap = "10px"
+  div.style.fontWeight = "bold";
+  return div;
+}
+
+function speakerDefaultButton(speaker) {
+  const button = document.createElement("button");
+  button.setAttribute("class", "primary");
+  button.textContent = "start";
+  button.style.minWidth = "70px";
+  button.addEventListener("click", () => {
+    speaker.stoped = !speaker.stoped;
+    button.textContent = speaker.stoped ? "start" : "stop";
+  });
+  return button;
+}
+
+function speakerNameContainer(speaker) {
+  const name = document.createElement("div");
+  name.textContent = speaker.name;
+  return name;
+}
+
+function speakerTimerContainer(speaker, id) {
+  const timer = document.createElement("div");
+  timer.setAttribute("id", id);
+  timer.setAttribute("class", "timer");
+  timer.textContent = speaker.timer.toLocaleTimeString();
+  return timer;
+}
+
+function inputContainer(ul) {
+  const container = document.createElement("div");
+  container.setAttribute("class", "input-container");
+  const label = inputLabel();
+  const input = getInput(ul);
+  label.append(input);
+  const button = inputButton(ul, input)
+  container.append(label);
+  container.append(button);
+  return container;
+}
+
+function inputLabel() {
+  const label = document.createElement("label");
+  label.setAttribute("for", "input");
+  label.textContent = "Neuer Redner ";
+  return label;
+}
+
+function getInput(ul) {
+  const input = document.createElement("input");
+  input.setAttribute("id", "input");
+  input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      addSpeaker(ul, input.value)
+      input.value = "";
+    }
+  });
+  return input;
+}
+
+function inputButton(ul, input) {
+  const button = document.createElement("button");
+  button.setAttribute("class", "primary");
+  button.textContent = "hinzufügen";
+  button.addEventListener("click", () => {
+    addSpeaker(ul, input.value)
+    input.value = "";
+  });
+  return button;
+}
+
+function titel() {
+  const titel = document.createElement("h1");
+  titel.textContent = "Rednerliste mit Zeitmessung";
+  return titel;
+}
+
+function subtitel() {
+  const subtitel = document.createElement("h3");
+  subtitel.textContent = "Rednerliste";
+  return subtitel;
+}
+
+function speakersContainer(ul) {
+  const speakersContainer = document.createElement("div");
+  speakersContainer.setAttribute("class", "speaker-container");
+  initSpeakers(ul);
+  speakersContainer.append(ul);
+  return speakersContainer;
+}
+
+function addSpeaker(ul, speakerName) {
+  let index = speakers.length;
+  let speaker = new Speaker(speakerName)
+  speakers.push(speaker);
+  const li = document.createElement("li");
+  const div = spreakerDefaultContainer();
+  div.append(speakerDefaultButton(speaker));
+  div.append(speakerNameContainer(speaker));
+  div.append(speakerTimerContainer(speaker, index));
+  li.append(div)
+  ul.append(li);
+}
+
+function addSecond(date) {
+  date.setSeconds(date.getSeconds() + 1);
   return date;
 }
 
 export default {
-  data() {
-    return {
-      input: "",
-      speakers: [],
-    };
-  },
-
-  methods: {
-    addSpeaker(name) {
-      console.log(document.getElementById("test"));
-      this.speakers.push(new Speaker(name));
-    },
-  },
   mounted() {
     const root = document.getElementById("root");
-
-    const titel = document.createElement("h1");
-    titel.textContent = "Rednerliste mit Zeitmessung";
-    root.append(titel);
-
-    const subtitel = document.createElement("h3");
-    subtitel.textContent = "Rednerliste";
-    root.append(subtitel);
-
-    const inputContainer = document.createElement("div");
-    inputContainer.setAttribute("class", "input-container");
-    const label = document.createElement("label");
-    label.setAttribute("for", "input");
-    label.textContent = "Neuer Redner ";
-    const input = document.createElement("input");
-    input.setAttribute("id", "input");
-    label.append(input);
-    const button = document.createElement("button");
-    button.setAttribute("class", "primary");
-    button.textContent = "hinzufügen";
-    button.addEventListener("click", () => {
-      speakers.push(new Speaker(input.value));
-      var li = document.createElement("li");
-      li.textContent = input.value;
-      ul.append(li);
-    });
-    inputContainer.append(label);
-    inputContainer.append(button);
-    root.append(inputContainer);
-
-    const speakerContainer = document.createElement("div");
-    speakerContainer.setAttribute("class", "speaker-container");
+    root.append(titel());
+    root.append(subtitel());
     const ul = document.createElement("ul");
-    for (let i = 0; i < speakers.length; i++) {
-      const li = document.createElement("li");
-      li.textContent = speakers[i].name;
-      ul.append(li);
-      const timer = document.createElement("div");
-      timer.setAttribute("id", i);
-      timer.setAttribute("class", "timer");
-      li.append(timer);
-      const button = document.createElement("button");
-      button.setAttribute("class", "primary");
-      button.textContent = "start";
-      button.addEventListener("click", () => {
-        speakers[i].stoped = !speakers[i].stoped;
-        button.textContent = speakers[i].stoped ? "start" : "stop";
-      });
-      li.append(button);
-    }
-    speakerContainer.append(ul);
-    root.append(speakerContainer);
-
-    initTimers();
+    root.append(inputContainer(ul));
+    root.append(speakersContainer(ul));
     setInterval(appTimer, 1000);
   },
 };
