a8cff7b8025798dacef1f0ddcf0fb89a13d49228
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-11-26 12:44:40 +0100

    es home implemented

---
+++ src/views/practices/06-es/ES-Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Übung 6: ECMAScript (ES)</h1>
+  <h3>Lernziele</h3>
+  <ol>
+    <li>ECMAScript-Konzepte erklären können</li>
+    <li>ECMAScript-Programme selbst erstellen können</li>
+    <li>Arrow Functions schreiben können</li>
+    <li>
+      Den Unterschied zwischen Functions und Arrow Functions beschreiben können
+    </li>
+    <li>Den Unterschied zwischen var und const / let beschreiben können</li>
+    <li>Klassenausdrücke und Mixins in ECMAScript programmieren können</li>
+    <li>Tagged Template Literals programmieren können</li>
+    <li>Destructuring einsetzen können</li>
+    <li>Den Spread-Operator einsetzen können</li>
+    <li>Das Iterationsprotokoll beschreiben und umsetzen können</li>
+    <li>Generatoren programmieren können</li>
+    <li>ES6 Module anwenden, d.h. exportieren und importieren können</li>
+    <li>
+      Collection Datentypen wie Map, Set, WeakMap und WeakSet anwenden können
+    </li>
+    <li>Proxies anwenden können</li>
+    <li>Promises programmieren können</li>
+    <li>Object APIs anwenden können</li>
+    <li>Deep Copy programmieren können</li>
+  </ol>
+</template>
