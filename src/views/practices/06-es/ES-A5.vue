ccb55f0285f766e36e600483385c9ce091b48d62
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-01 10:02:16 +0100

    ES A5 implemented

--- src/views/practices/06-es/ES-A5.vue
+++ src/views/practices/06-es/ES-A5.vue
@@ -1,11 +1,70 @@
 <template>
-  <div></div>
+  <h1>DeepCopy</h1>
+  <p>
+    In dieser Aufgabe wurde eine rekursive Funktion deepCopy(struct) als
+    ES6-Ausdruck entwickelt, sodass beliebig geschachtelte Arrays und Objekte
+    tiefenkopiert werden können.
+  </p>
+  <div class="container">
+    <div class="object-container">
+      <h3>Das originale Objekt</h3>
+      <pre><code>{{ me }}</code></pre>
+    </div>
+    <div class="object-container">
+      <h3>Die Kopie</h3>
+      <pre><code>{{ copy }}</code></pre>
+    </div>
+  </div>
+  <br>
+  <div>Sind die beiden Objekte gleich? Dazu wird "=" Operator aufgerufen.</div>
+  <div>Ergebnis: <strong>{{ equal }}</strong></div>
 </template>
 
-<script>
+<script setup>
+let me = {
+    name: "Ahmed",
+    address: {
+      city: "Sankt Augustin",
+      app: {
+        number: 53757,
+        floor: 2,
+        name: undefined,
+        foo: null,
+      },
+    },
+    modules: ["Web Engineering", "Software Engineering"],
+  };
+
+let deepCopy = function (object) {
+  return object == null
+    ? undefined
+    : ["string", "number", "boolean", "undefined", "function"].includes(
+        typeof object
+      )
+    ? object
+    : Array.isArray(object)
+    ? object.map((o) => deepCopy(o))
+    : Object.fromEntries(Object.entries(object).map((o) => deepCopy(o)));
+};
+
+let copy = deepCopy(me);
+
+let equal = me == copy;
 
 </script>
   
 <style lang="scss" scoped>
 
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.object-container {
+  border: solid;
+  padding: 1vw;
+  border-radius: 5px;
+  margin: 1vw
+}
 </style>
