860cd6c742390cc27c106f735933ff2aa2bd903f
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-01 10:06:24 +0100

    AJ Home vue implemented

---
+++ src/views/practices/08-aj/AJ-Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Übung 8: Asynchrones JavaScript</h1>
+  <h3>Lernziele</h3>
+  <ol>
+    <li>Die Metamorphose des WWW erklären können</li>
+    <li>Kategorienfehler des WWW erklären können</li>
+    <li>Event-Loop des Browsers erklären können</li>
+    <li>Mit Ajax programmieren können</li>
+    <li>Mit Promise programmieren können</li>
+    <li>Mit async / await programmieren können</li>
+    <li>Mit Events programmieren können</li>
+    <li>Nebenläufige Worker programmieren können</li>
+  </ol>
+</template>
