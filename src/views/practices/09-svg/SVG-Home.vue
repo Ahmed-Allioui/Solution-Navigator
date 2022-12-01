2c95ccc8fd895097dbee4aad7bbe4e7b9410db70
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-01 11:31:19 +0100

    SVG home vue created

---
+++ src/views/practices/09-svg/SVG-Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Übung 9: Scalable Vector Graphics (SVG)</h1>
+  <h3>Lernziele</h3>
+  <ol>
+    <li>Konzepte von Scalable Vector Graphics (SVG) beschreiben können.</li>
+    <li>Eigene SVG-Grafiken von Hand implementieren können.</li>
+    <li>SVG-Grafiken animieren können.</li>
+    <li>SVG-Grafiken mit Interaktion versehen können.</li>
+    <li>
+      Transformationen, Transitionen und Animationen in CSS schreiben können.
+    </li>
+    <li>Interaktive SVG-Elemente implementieren können.</li>
+  </ol>
+</template>
