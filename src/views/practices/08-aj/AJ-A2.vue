e3e99afd244d293ea83544c0b388b2489205c762
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-01 10:05:42 +0100

    AJ A2 vue implemented

---
+++ src/views/practices/08-aj/AJ-A2.vue
@@ -0,0 +1,25 @@
+<template>
+  <AJA1Vue titel="async / await" usedTechnic="async / await" :importTexts="importTexts" />
+</template>
+
+<script setup>
+import AJA1Vue from "./AJ-A1.vue";
+// eslint-disable-next-line
+async function importTexts(merge) {
+  let arrays = []
+  let promiseA = fetch("/ressource/aj/A.txt");
+  let promiseB = fetch("/ressource/aj/B.txt");
+
+  arrays.push(await generateTextArray(await promiseA));
+  arrays.push(await generateTextArray(await promiseB));
+  merge(arrays);
+}
+
+async function generateTextArray(response) {
+  let text = (await response.text());
+  return text.split(/\r?\n/);
+}
+</script>
+  
+<style lang="scss" scoped>
+</style>
