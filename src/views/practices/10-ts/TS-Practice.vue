1e2560004f1950837aac09f42280caaec377bd25
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-01 11:33:35 +0100

    TS practice vue created

---
+++ src/views/practices/10-ts/TS-Practice.vue
@@ -0,0 +1,19 @@
+<template>
+    <AppTask :tabs="routes"/>
+</template>
+
+<script>
+import AppTask from '../../../components/task-component/App-Task.vue'
+import routes from '../10-ts/ts-routes'
+
+export default {
+    data() {
+        return {
+            routes: routes
+        }
+    },
+    components: {
+        AppTask
+    }
+};
+</script>
