5735ee2c351e2f31b770bff3cf4262b6b302e590
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-01 10:04:49 +0100

    FP Home implemented

---
+++ src/views/practices/07-fp/FP-Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Übung 7: Funktionale Programmierung</h1>
+  <h3>Lernziele</h3>
+  <ol>
+    <li>In JavaScript funktional programmieren können</li>
+    <li>Arrow Functions schreiben können</li>
+    <li>
+      Den Unterschied zwischen Functions und Arrow Functions beschreiben können
+    </li>
+    <li>Closures erklären können</li>
+    <li>Closures programmieren können</li>
+    <li>Keyword-Parameter einsetzen können</li>
+    <li>Rest-Parameter verwenden können</li>
+    <li>
+      Mit apply , call , bind arbeiten und damit die this -Bindung ändern können
+    </li>
+    <li>Den Function-Konstruktor einsetzen können</li>
+    <li>Fabrikfunktionen in JavaScript schreiben können</li>
+    <li>Ausdrücke für Funktionen, Objekte und Klassen schreiben können</li>
+    <li>filter - map - reduce -Ketten programmieren können</li>
+    <li>
+      find , findIndex , some und every statt for -Schleifen einsetzen können
+    </li>
+    <li>Auf MDN Funktionen nachschlagen können</li>
+  </ol>
+</template>
