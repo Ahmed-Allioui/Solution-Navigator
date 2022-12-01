f1bc5458b28e33bff7bb1e260bb0bb35a2a901df
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-01 11:32:38 +0100

    TS Home vue created

---
+++ src/views/practices/10-ts/TS-Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Übung 10: TypeScript</h1>
+  <h3>Lernziele</h3>
+  <ol>
+    <li>Die Konzepte von TypeScript kennen und anwenden können.</li>
+    <li>Mit TypeScript Typfehler finden können.</li>
+    <li>In TypeScript programmieren können.</li>
+    <li>In TypeScript Deno-Anwendungen schreiben können.</li>
+  </ol>
+</template>
