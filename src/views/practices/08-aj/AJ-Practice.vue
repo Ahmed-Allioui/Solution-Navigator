84515aa9ca4a4c3b703f34b0e46f9b62192e675b
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-01 10:06:37 +0100

    AJ Practices vue implemented

---
+++ src/views/practices/08-aj/AJ-Practice.vue
@@ -0,0 +1,19 @@
+<template>
+    <AppTask :tabs="routes"/>
+</template>
+
+<script>
+import AppTask from '../../../components/task-component/App-Task.vue'
+import routes from '../08-aj/aj-routes'
+
+export default {
+    data() {
+        return {
+            routes: routes
+        }
+    },
+    components: {
+        AppTask
+    }
+};
+</script>
