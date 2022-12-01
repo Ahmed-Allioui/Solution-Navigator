70b1d22eacdebbf345bc7c2028cd1f56a1b11f86
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-01 10:05:12 +0100

    FP Practices vue implemented

---
+++ src/views/practices/07-fp/FP-Practice.vue
@@ -0,0 +1,19 @@
+<template>
+    <AppTask :tabs="routes"/>
+</template>
+
+<script>
+import AppTask from '../../../components/task-component/App-Task.vue'
+import routes from '../07-fp/fp-routes'
+
+export default {
+    data() {
+        return {
+            routes: routes
+        }
+    },
+    components: {
+        AppTask
+    }
+};
+</script>
