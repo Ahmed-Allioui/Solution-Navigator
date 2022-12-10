6ecda11546d902c86c1e230adbe56a263b550a07
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-10 10:42:54 +0100

    implemented TS task 3

--- src/views/practices/10-ts/TS-A3.vue
+++ src/views/practices/10-ts/TS-A3.vue
@@ -1,5 +1,15 @@
 <template>
   <h1>TypeScript auf Deno</h1>
+  <div class="container">
+    <div class="text-container"><strong>Server.ts</strong> ist ein auf Deno ablauffähiges Programm, dass mit deno
+      run --allow-net server.ts gestartet werden kann, das die COVID-19-
+      Fallzahlen in Deutschland einliest, Minimum, Maximum, Durchschnitt
+      und Summe berechnet und diese auf dem Browser zeigt. Dazu muss der Port <strong>8000</strong> im Browser aufgerufen werden.</div>
+    <div class="img-container">
+      <div><strong>Server.ts</strong></div>
+      <img src="./code.jpeg" alt="Code">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,5 +17,18 @@
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 40px;
 
+  .img-container {
+    max-width: 600px;
+
+    img {
+      border-radius: 5px;
+    }
+  }
+}
 </style>
