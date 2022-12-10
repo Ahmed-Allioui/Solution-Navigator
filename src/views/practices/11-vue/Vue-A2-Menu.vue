b16945b28c0685972e9bf37a1b3ce42836eea52b
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-12-10 12:12:25 +0100

    implemented task 2

---
+++ src/views/practices/11-vue/Vue-A2-Menu.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="menu-container">
+        <button class="primary" v-for="(item, index) in items" :key="index">
+            {{ item }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: ['Home', 'Über uns', 'Kontakt']
+        }
+    }, 
+    props: {
+        isVertical: {
+            type: Boolean,
+            default: false
+        }
+    },
+    mounted() {
+        if(this.isVertical) document.getElementById('menu-container').style.flexDirection = 'column'  	
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+#menu-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    column-gap: 0.5vw;
+    background-color: var(--primary);
+    width: fit-content;
+
+    button {
+        padding-left: 2vw;
+        padding-right: 2vw;
+    }
+}
+</style>
--- src/views/practices/11-vue/Vue-A2.vue
+++ src/views/practices/11-vue/Vue-A2.vue
@@ -1,9 +1,24 @@
 <template>
   <h1>Menü-Komponente</h1>
+  <p>In dieser Aufgabe wurde eine flexible Vue.js Single File Component für
+    Menüs geschrieben. Dann wurde die Komponente in dieser Webseite zweimal angewandt,
+    einmal horizontal, das andere Mal vertikal.</p>
+  <p class="question">Lösung:</p>
+  <p>Diese Komponente ist standardmäßig horizontal. Also wenn ein vertikaler
+    Menu benötigt wird, soll nur den Flag <strong>isVertical</strong> auf <strong>True</strong> gesetzt werden.</p>
+  <br>
+  <div>
+    <MenuVue />
+  </div>
+  <br>
+  <div>
+    <MenuVue isVertical="true" />
+  </div>
+
 </template>
 
 <script setup>
-
+import MenuVue from './Vue-A2-Menu.vue';
 </script>
   
 <style lang="scss" scoped>
