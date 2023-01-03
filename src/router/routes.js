d6d6dbd6a4e38bb3968d80ba5cd0d899c5590292
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2023-01-03 15:23:21 +0100

    implemented contact page

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,9 @@
 import Home from '../views/home-vue/App-Home.vue'
 import About from '../views/about-vue/App-About.vue'
+import Contact from '../views/contact-vue/App-Contact.vue'
 import Practices from '../views/practices/App-Practices.vue'
 import PageNotFound from '../views/page-not-found/App-PageNotFound.vue'
-import practicesRoutes from '../views/practices/practices-routes'
+import practicesRoutes from '../views/practices/practices-routes.js'
 
 const routes = [
     {
@@ -21,7 +22,7 @@ const routes = [
     },
     {
         path: '/contact',
-        component: About,
+        component: Contact,
         name: 'contact',
         titel: 'Kontakt',
         icon: 'mail'
---
+++ src/views/contact-vue/App-Contact.vue
@@ -0,0 +1,75 @@
+<template>
+  <main class="contact-page">
+    <StandardPageComponent title="Kontakt">
+      <div class="contact-container">
+        <div class="text-container">
+          <div><strong>André Kless:</strong></div>
+          <br>
+          <div>
+            <ul>
+              <li class="icon-container">
+                <div>
+                  <Cellphone class="icon" /> <a href="tel:+49 2241 865 695">+49 2241 865 695</a>
+                </div>
+              </li>
+              <li class="icon-container">
+                <div>
+                  <Message class="icon" /> <a href="mailto:andre.kless@h-brs.de">andre.kless@h-brs.de</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="text-container">
+          <div><strong>Ahmed Allioui:</strong></div>
+          <br>
+          <div>
+            <ul>
+              <li class="icon-container">
+                <div>
+                  <Cellphone class="icon" /> <a href="tel:+49 1789 045 629">+49 1789 045 629</a>
+                </div>
+              </li>
+              <li class="icon-container">
+                <div>
+                  <Message class="icon" /> <a href="mailto:ahmed.allioui@smail.inf.h-brs.de">ahmed.allioui@smail.inf.h-brs.de</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </StandardPageComponent>
+  </main>
+</template>
+
+<script setup>
+import StandardPageComponent from '@/components/atoms/StandardPageComponent.vue';
+import { Cellphone, Message } from '@element-plus/icons-vue'
+</script>
+
+<style lang="scss" scoped>
+.contact-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  
+}
+
+.text-container {
+  text-align: start;
+  font-size: calc(15px + 0.2vw);
+  min-width: 400px;
+}
+
+.icon-container {
+  display: flex;
+  flex-direction: row;
+}
+
+.icon {
+  width: 20px;
+  vertical-align: middle;
+}
+</style>
