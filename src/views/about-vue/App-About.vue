3e2198afcc2ff3aa5aae5462f86334a83e7619f0
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2023-01-03 15:23:06 +0100

    implemented about page

--- src/views/about-vue/App-About.vue
+++ src/views/about-vue/App-About.vue
@@ -1,6 +1,29 @@
 <template>
-    <main class="about-page">
-      <h1>About</h1>
-      <p>This is the About Page</p>
-    </main>
-  </template>
+  <main class="about-page">
+    <StandardPageComponent title="Wer sind wir ?">
+      <div class="text-container">Dieses Projekt wurde im Rahmen der
+        Veranstaltung <strong>Einführung in
+          Web Engineering</strong> in der <strong>Hochschule Bonn-Rhein-Sieg</strong> entwickelt.
+      </div>
+      <div class="text-container">
+        <strong>André Kless:</strong> M.Sc. Lehrkraft für
+        besondere Aufgaben im Fachbereich Informatik in der Hochschule Bonn-Rhein-Sieg,
+        Grantham-Allee 20, 53757 Sankt Augustin.
+      </div>
+      <div class="text-container">
+        <strong>Ahmed Allioui:</strong> Bachelorstudienrender im Fachbereich Informatik an der Hochschule Bonn-Rhein-Sieg,
+        Grantham-Allee 20, 53757 Sankt Augustin.
+      </div>
+    </StandardPageComponent>
+  </main>
+</template>
+
+<script setup>
+import StandardPageComponent from '@/components/atoms/StandardPageComponent.vue';
+</script>
+
+<style lang="scss" scoped>
+.text-container {
+  font-size: calc(15px + 0.2vw);
+}
+</style>
